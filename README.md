28ecad7ee1bb2a994c31d2f9528f1e4856a2c12b
Tuan Pham <tpham@ancestry.com>
2018-06-12 08:01:28 -0700

    Setup deployment process

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Red Velvet
 A Web UI for Redis.
 
+## Features
+- SSL/TLS support
+
+## Install
+```bash
+pip3 install red-velvet 
+```
+
+## Run
+```bash
+export REDIS_URIS="local::redis://localhost:6379,dev::rediss://dev.redis.com/0"
+red-velvet
+```
+
+The server should be running on <http://localhost:8000> by default.
+
+## Configurations
+Configurations are done through environmental variables
+- `HOST`: [OPTIONAL] (Default: 0.0.0.0) The hostname of the server
+- `PORT`: [OPTIONAL] (Default: 8000) The port of the server
+- `NUM_WORKERS`: [OPTIONAL] (Default: 1) The number of workers to start up
+- `REDIS_URIS`: Comma separated list of [redis URIs](https://metacpan.org/pod/URI::redis).
+
+## Docker
+```bash
+docker run -d \
+           -e REDIS_URIS="dev::rediss://dev.redis.com/0" \
+           -p 8000:8000 \
+           --name red-velvet \
+           ttpham0111/red-velvet
+```
+
 ## TODO:
 - Fix hacky import of toggle button plugin
 - Update in memory instead of calling get*
--- setup.py
+++ setup.py
@@ -2,24 +2,32 @@ from setuptools import setup, find_packages
 
 setup(
     name='red-velvet',
-
     version='0.0.0',
-
+    author='Tuan Pham',
     description='Redis Viewer Web UI',
-
     url='https://github.com/ttpham0111/red-velvet',
 
+    classifiers=(
+        'Environment :: Web Environment',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Utilities'
+    ),
+
     entry_points={
-        'console_scripts': [
-            'red-velvet = redvelvet.app:start'
-        ]
+        'console_scripts': (
+            'red-velvet = redvelvet.app:start',
+        )
     },
 
     packages=find_packages(exclude=['test']),
     include_package_data=True,
 
-    install_requires=[
+    install_requires=(
         'sanic',
-        'aioredis'
-    ]
+        'aioredis',
+    )
 )
