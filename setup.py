d7c653fad250de7477e7e24a0c7b9fc460af34e9
Tuan Pham <tpham@ancestry.com>
2018-06-12 08:25:18 -0700

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='red-velvet',
-    version='0.0.0',
+    version='0.0.1',
     author='Tuan Pham',
     description='Redis Viewer Web UI',
     url='https://github.com/ttpham0111/red-velvet',
