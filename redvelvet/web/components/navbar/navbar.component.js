630662fa967cd9a3330395a670d7357375e0984e
Tuan Pham <tpham@ancestry.com>
2018-06-13 15:35:55 -0700

    Chrome doesn't scale height with width for image

--- redvelvet/web/components/navbar/navbar.component.js
+++ redvelvet/web/components/navbar/navbar.component.js
@@ -2,7 +2,7 @@ Vue.component('r-navbar', {
   template: `
     <b-navbar type="dark" variant="dark" fixed="top">
       <b-navbar-brand class="mx-auto d-flex" href="#">
-        <img src="public/red-velvet.png" width="30px" />
+        <img src="public/red-velvet.png" width="30px" height="30px" />
         <span class="ml-1">Red Velvet</span>
       </b-navbar-brand>
     </b-navbar>
